080221b82abcfbdd11e59d55d6c02c01710449e1
Dobrelya22 <ostap.andrey21@gmail.com>
2024-04-28 14:13:34 +0300

    Resolved the assigned task

--- Control-test/Program.cs
+++ Control-test/Program.cs
@@ -2,8 +2,24 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, world!");
+        string[] sourceArray = { "Hello", "2", "world", ":-)" };
+        string[] resultArray = new string[sourceArray.Length];
+        
+        int resultIndex = 0;
+        foreach (var item in sourceArray)
+        {
+            if (item.Length <= 3)
+            {
+                resultArray[resultIndex++] = item;
+            }
+        }
+
+        // Обрезаем результирующий массив до фактического размера с результатами
+        Array.Resize(ref resultArray, resultIndex);
+
+        // Вывод результирующего массива
+        Console.WriteLine("[" + string.Join(", ", sourceArray) + "] → [" + string.Join(", ", resultArray) + "]");
     }
 }
